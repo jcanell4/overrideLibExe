cafd41f5338c6c71293d69f059f303922bad4659
rclaver <rclaver@xtec.cat>
2017-11-29 14:01:49 +0100

    corregir error directorio INC

--- ioc_ajaxrest.php
+++ ioc_ajaxrest.php
@@ -5,7 +5,7 @@
  * per a ns_tree_rest, ns_mediatree_rest, list_projects_rest, list_templates_rest
  * @author Josep Cañellas <jcanell4@ioc.cat>
  */
-if (!defined('DOKU_INC')) define('DOKU_INC', realpath(dirname(__FILE__)."/../") . '/');
+if (!defined('DOKU_INC')) define('DOKU_INC', realpath(dirname(__FILE__)."/../../") . '/');
 require_once DOKU_INC."lib/plugins/ajaxcommand/ajaxClasses.php";
 
 $inst = ajaxRest::Instance();
